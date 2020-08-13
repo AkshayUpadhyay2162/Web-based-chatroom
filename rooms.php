175b629f5d332c0f5793b38d6eae0d2c844245ba
= <=>
2020-08-13 16:51:10 +0530

    Almost done

--- rooms.php
+++ rooms.php
@@ -102,10 +102,7 @@ if ($result) {
     <div class="container bg-transparent">
       <h2 class="text-center text-white mb-5">Chat Messages - <?php echo $roomname ?></h2>
       <div class="container">
-        <div class="anyClass" style="height: 350px; overflow-y:scroll">
-          <img src="/w3images/bandmember.jpg" alt="Avatar" style="width:100%;">
-          <p>Hello. How are you today?</p>
-          <span class="time-right">11:00</span>
+        <div class="anyClass" style="height: 350px; overflow-y:scroll">          
         </div>
       </div>
     </div>
@@ -153,6 +150,17 @@ if ($result) {
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
   <script>
+// checking for messages 
+setInterval(checkFunction,1000);
+
+function checkFunction(){
+  $.post("htcon.php",{room: '<?php echo $roomname ?>'},
+  function(data,status){
+    document.getElementsByClassName('anyClass')[0].innerHTML = data;
+  }
+  )
+
+}
 
 var input = document.getElementById("msg");
 input.addEventListener("keyup", function(event) {
