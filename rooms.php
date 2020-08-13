f73b910793ef436abb4871b76eba203d337f70d1
= <=>
2020-08-13 23:48:33 +0530

    Modification 3.1

--- rooms.php
+++ rooms.php
@@ -95,7 +95,7 @@ if ($result) {
 <body id="page-top">
   <nav class="navbar navbar-expand-lg navbar-light fixed-top mx-auto" id="mainNav">
     <div class="container bg-transparent">
-      <a class="navbar-brand js-scroll-trigger" href="index.php">iChat.com</a>
+      <a class="navbar-brand js-scroll-trigger" href="index.html">iChat.com</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto my-2 my-lg-0">
